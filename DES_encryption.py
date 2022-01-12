c61198b5bfebd00eb5979235acbc1754173318ae
Abhishek singh <35148084+abhi0697@users.noreply.github.com>
2022-01-12 10:22:13 +0100

    Update DES_encryption.py

--- DES_encryption.py
+++ DES_encryption.py
@@ -1,16 +1,15 @@
 from check_pwned import pwnedpassword
-from Crypto.Cipher import DES
+from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
-# Encrypting the generated and validated password with DES CBC mode
-IV = get_random_bytes(8)
-def des_cbc_encrypt(key1, plaintext, mode=DES.MODE_CBC): 
+# Encrypting the generated and validated password with AES CBC mode
+IV = get_random_bytes(16)
+def aes_cbc_encrypt(key1, plaintext, mode=AES.MODE_CBC): 
     try:
-        pad = lambda s: s + (8 - len(s) % 8) * chr(8 - len(s) % 8)
+        pad = lambda s: s + (16 - len(s) % 16) * chr(16 - len(s) % 16)
         plaintext=pad(plaintext)
-        des = DES.new(key1, mode, IV) 
-        new_data = des.encrypt(plaintext.encode('utf8')) 
+        aes = AES.new(key1, mode, IV) 
+        new_data = aes.encrypt(plaintext.encode('utf8')) 
         pwnedpassword(new_data)
-        # print(new_data)
         return new_data 
     except Exception as e:
         print(e)
